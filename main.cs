f26f9c167668a842ceedf27deb2112be6d1eefd6
Nikola Grujčić <>
2021-04-03 19:41:42 +0000

    Uneo sam sve boje teritorija

--- main.cs
+++ main.cs
@@ -4,14 +4,16 @@ class MainClass
 {
     static char[,] PretvaracTxtMapeUMatricu(string ImeFajla)
     {
-        char[,] Mapa = new char[50, 250];
+      
+        char[,] Mapa = new char[50, 200];
+        for(int i=0;i<Mapa.GetLength(0);i++)for(int j=0;j<Mapa.GetLength(1);j++)Mapa[i,j]='#';
         string linijaMape;
         StreamReader CitacMape = new StreamReader(ImeFajla);
         int brojReda = 0;
         while (!CitacMape.EndOfStream)
         {
             linijaMape = CitacMape.ReadLine();
-            for (int i = 0; i < linijaMape.Length; i++) Mapa[brojReda, i] = linijaMape[i];
+            for (int i = 0; i < 199; i++) Mapa[brojReda, i] = linijaMape[i];
             brojReda++;
         }
         CitacMape.Close();
@@ -24,7 +26,7 @@ class MainClass
             for (int j = 0; j < Mapa.GetLength(1); j++)
             {
 
-                int broj = Mapa[i, j];
+                int broj = Mapa[i, j] - 'A';
 
                 if (Mapa[i, j] == '#')
                 {
@@ -51,16 +53,40 @@ class MainClass
                     Console.BackgroundColor = ConsoleColor.Red;
                     Console.ForegroundColor = ConsoleColor.Red;
                 }
+                else if (TestVrednosti[0, broj] == 3)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkYellow;
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                }
+                else if (TestVrednosti[0, broj] == 4)
+                {
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                }
+                else if (TestVrednosti[0, broj] == 5)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkGreen;
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                }
+                else if (TestVrednosti[0, broj] == 6)
+                {
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
                 Console.Write(Mapa[i, j]);
             }
             Console.WriteLine();
         }
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Black;
     }
     public static void Main(string[] args)
     {
-        int[,] TestVrednosti = new int[4, 100];
-        for (int i = 0; i < TestVrednosti.GetLength(1); i++) TestVrednosti[0, i] = i % 6;
+        int[,] TestVrednosti = new int[4, 20];
+        for (int i = 0; i < TestVrednosti.GetLength(1); i++) TestVrednosti[0, i] = i % 7;
         char[,] Mapa = PretvaracTxtMapeUMatricu("MAPA1");
         IspisMape(Mapa, TestVrednosti);
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Black;
     }
 }
